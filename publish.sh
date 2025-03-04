b951f60b9320183291f08cb7d3215e7bc434cc31
yangchunpeng <ruochengflag@163.com>
2025-03-04 15:53:11 +0800

    更新脚本自动化提交

---
+++ publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 提示用户输入提交信息
+read -p "请输入提交信息: " commit_message
+
+# 执行 git 命令
+git add .
+git commit -m "$commit_message"
+git push origin master
+
+# 询问用户是否发布npm包
+read -p "是否发布npm包? (y/n): " publish_npm
+
+if [[ $publish_npm == 'y' ]]; then
+  # 执行 npm 相关命令
+  npm run build:lib
+  npm config set registry https://registry.npmjs.org
+  npm publish
+  npm config set registry https://registry.npmmirror.com
+fi
+
+# 提示用户脚本执行完成
+echo "代码执行完成。"
